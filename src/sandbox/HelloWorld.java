a92b1a4b8555fe4d3ed5e3516cf07dabbad47e3d
Brendan <brendan.galea@mail.mcgill.ca>
2015-11-21 22:21:39 -0500

    Initial commit

---
+++ src/sandbox/HelloWorld.java
@@ -0,0 +1,12 @@
+package sandbox;
+
+/**
+ *
+ * Created by Brendan Galea on 11/21/2015.
+ */
+public class HelloWorld {
+
+  public static void main(String args[]) {
+    System.out.println("Hello World");
+  }
+}
